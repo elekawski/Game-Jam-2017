a69547483d0c21c7c1e73fc23b9bdc477f50e254
Josh Chartrand <JoshChartrand@cunet.carleton.ca>
2017-01-21 14:42:12 -0500

    Added a couple assets and simple item pickup

---
+++ Game Jam 2017/Assets/Scripts/GrabItem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabItem : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "CharacterRobotBoy")
+        {
+            GetComponent<AudioSource>().Play();
+            Destroy(GetComponent<BoxCollider2D>());
+            GetComponent<Renderer>().enabled = false;
+            Destroy(gameObject, 0.47f);// Destroy the object -after- the sound played
+        }
+    }
+}
